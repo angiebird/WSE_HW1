b86256093e6f63f66e1ba4b67c0796705ede1fb3
Angie Chiang <angieyayabird@gmail.com>
2014-10-05 01:47:57 -0400

    Add normalization to EvaluatorNDCG.java

--- src/edu/nyu/cs/cs2580/EvaluatorNDCG.java
+++ src/edu/nyu/cs/cs2580/EvaluatorNDCG.java
@@ -19,13 +19,22 @@ public class EvaluatorNDCG extends Evaluator{
         ArrayList<ScoredDocument > query_rank){
 
         double DCG = 0;
+        double MAX_DCG = 0; // The maximum DCG score associates to the query
+        int j = 1;
         for(int i = 1; i <= K; i++){
-            ScoredDocument doc = query_rank.get(i);
+            ScoredDocument doc = query_rank.get(i-1);
             if(query_relevance.containsKey(doc._did)){
                 double rel = query_relevance.get(doc._did);
                 DCG += rel/(Math.log(i+1)/Math.log(2));
+                MAX_DCG += 10/(Math.log(j+1)/Math.log(2));
+                j++;
             }
         }
-        return DCG;
+        if(MAX_DCG == 0.0){
+            return 0;
+        }
+        else{
+            return DCG/MAX_DCG;
+        }
     }
 }
