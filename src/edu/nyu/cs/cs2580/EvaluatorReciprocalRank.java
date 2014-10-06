5cf8383626edc5ab8fe5828472a50939ab9f505c
Angie Chiang <angieyayabird@gmail.com>
2014-10-05 23:07:28 -0400

    consider no relevance in EvaluatorReciprocalRank.java

--- src/edu/nyu/cs/cs2580/EvaluatorReciprocalRank.java
+++ src/edu/nyu/cs/cs2580/EvaluatorReciprocalRank.java
@@ -14,16 +14,21 @@ public class EvaluatorReciprocalRank extends Evaluator{
         Iterator<ScoredDocument> it = query_rank.iterator();
         
         double i = 0;
+        int flg = 0;
         while(it.hasNext()){
             i++;
             ScoredDocument doc = it.next();
             if(query_relevance.containsKey(doc._did)){
                 double rel = query_relevance.get(doc._did);
                 if (rel >= 5.0){
+                    flg = 1;
                     break;
                 }
             }
         }
-        return 1.0/i;
+        if(flg > 0)
+            return 1.0/i;
+        else
+            return 0;
     }
 }
