7ce95d2212a6ebd8d5f33de57b01ed1a8acacdd3
Angie Chiang <angieyayabird@gmail.com>
2014-10-05 22:42:26 -0400

    re-implement EvaluatorPrecisionRecall.java

--- src/edu/nyu/cs/cs2580/EvaluatorPrecisionRecall.java
+++ src/edu/nyu/cs/cs2580/EvaluatorPrecisionRecall.java
@@ -16,12 +16,17 @@ public class EvaluatorPrecisionRecall extends Evaluator{
 	public ArrayList<Double> evaluate2(String query,
 			HashMap<Integer, Double> query_relevance,
 			ArrayList<ScoredDocument> query_rank) {
-        double lv = 0.1;
         double RR = 0.0;
         double R = 0.0;
-        double precision = 0.0;
-        ArrayList<Double> precisionLs = new ArrayList<Double>();
-        precisionLs.add(0.0); // precision at recall 0.0 is 0.0
+        int resultsNum = 11;
+
+        ArrayList<Double> lv = new ArrayList<Double>();
+        ArrayList<Double> precisionLs= new ArrayList<Double>();
+        for(int i = 0; i < resultsNum; i++){
+            lv.add(i*0.1);
+            precisionLs.add(0.0);
+        }
+
 
         // Calculate the number of all revelance documents;
         for(int i = 0; i < query_rank.size(); i++){
@@ -34,16 +39,36 @@ public class EvaluatorPrecisionRecall extends Evaluator{
             }
         }
 
-        for(int i = 0; i < query_rank.size(); i++){
-            ScoredDocument doc = query_rank.get(i);
-            if(query_relevance.containsKey(doc._did)){
-                double rev = query_relevance.get(doc._did);
-                if(rev >= 5.0){
-                    RR++;
-                    precision = RR/R;
-                    if(precision >= lv){
-                        precisionLs.add(precision);
-                        lv = lv + 0.1;
+        if(R > 0){
+            int lvi = 0;
+            double prec= 0.0;
+            double prevPrec= 0.0;
+            double recall = 0.0;
+            double prevRecall = 0.0;
+            for(int i = 0; i < query_rank.size(); i++){
+                ScoredDocument doc = query_rank.get(i);
+                if(query_relevance.containsKey(doc._did)){
+                    double rev = query_relevance.get(doc._did);
+                    if(rev >= 5.0){
+                        RR++;
+
+                        prevPrec= prec;
+                        prec = RR/(i+1);
+
+                        prevRecall = recall;
+                        recall = RR/R;
+                        //System.out.println("R: " + RR/R + " P: " + RR/(i+1));
+
+                        double level = lv.get(lvi);
+                        while(level >= prevRecall && level <= recall && lvi < resultsNum){
+                            // interleaving
+                            double precision = (prevPrec*(recall - level) + prec*(level - prevRecall))/(recall - prevRecall);
+                            precisionLs.set(lvi, precision);
+                            //System.out.println(level + " " + precision);  
+                            lvi++;
+                            if(lvi < resultsNum)
+                                level = lv.get(lvi);
+                        }
                     }
                 }
             }
